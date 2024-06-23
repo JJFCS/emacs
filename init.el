0205be5d75872504917efe66dfffa98651ac20b9
onncera <onncera@localhost.com>
2024-06-23 22:46:33 +0800

    improving emacs

---
+++ init.el
@@ -0,0 +1,15 @@
+(org-babel-load-file (expand-file-name "onncera-emacs.org" user-emacs-directory))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Iosevka Nerd Font Mono" :foundry "nil" :slant normal :weight regular :height 140 :width normal)))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(wombat))
+ '(package-selected-packages
+   '(org-bullets rainbow-delimiters doom-modeline moe-theme modus-themes leuven-theme gruber-darker-theme doom-themes magit undo-tree company wgrep vertico marginalia embark-consult orderless)))
